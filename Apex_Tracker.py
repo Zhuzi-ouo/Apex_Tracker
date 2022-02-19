746c76dd558708ca17c3e802920b13271645ccf5
Zhuzi <85818541+Zhuzi-ouo@users.noreply.github.com>
2022-02-19 16:32:58 +0800

    Update Apex_Tracker.py

--- Apex_Tracker.py
+++ Apex_Tracker.py
@@ -1,11 +1,10 @@
-User=input ("Enter Your Apex Username:")
 import requests
 from bs4 import BeautifulSoup
-
-response=requests.get(f"https://tracker.gg/apex/profile/origin/{User}/overview")
-soup=BeautifulSoup(response.text,"html.parser")
-Apextracker=soup.find_all("span",class_="value")
-Apexrank=soup.find_all("span",class_="mmr")
-print ("Level",Apextracker[0].string)
-print ("Battle Royale Rank",Apexrank[0].text)
-print ("Arenas Rank",Apexrank[1].text)
+def AT(User):
+    response=requests.get(f"https://tracker.gg/apex/profile/origin/{User}/overview")
+    soup=BeautifulSoup(response.text,"html.parser")
+    Apextracker=soup.find_all("span",class_="value")
+    Apexrank=soup.find_all("span",class_="mmr")
+    AT.Level=Apextracker[0].string
+    AT.BRR=Apexrank[0].text
+    AT.AR=Apexrank[1].text
